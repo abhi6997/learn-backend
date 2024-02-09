ac9fad4fd8bbdcbd5f8cfb0df6f9490134b7c8cd
Abhishek Shende <imabhi6997@gmail.com>
2024-02-09 23:50:52 +0530

    created multiple routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { watchFile } from "fs";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -277,46 +278,173 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "user details updated successfully"));
 });
 
-const updateUserAvatar = asyncHandler(async(req,res)=>{
-    const avatarLocalPath = req.file?.path
-    if (!avatarLocalPath){
-      throw new ApiError(400,"Avatar file is missing")
-    }
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is missing");
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
 
-    if (!avatar.url){
-      throw new ApiError(400,"Error while uploading file on cloudinary")
-    }
+  if (!avatar.url) {
+    throw new ApiError(400, "Error while uploading file on cloudinary");
+  }
 
-    const user = await User.findByIdAndUpdate(
-      req.user?._id,{$set:{
-        avatar: avatar.url
-      }},{new:true}
-    ).select("-password -refreshToken")
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
+    },
+    { new: true }
+  ).select("-password -refreshToken");
 
-    return res
+  return res
     .status(200)
-    .json(new ApiResponse(200,user,"Avatar Update successfully"))
-})
+    .json(new ApiResponse(200, user, "Avatar Update successfully"));
+});
 
-const updateUserCoverImage = asyncHandler(async(req,res)=>{
-  const coverImageLocalPath = req.file?.coverImage[0]?.path
-  if (!coverImageLocalPath){
-    throw new ApiError(400,"coverImage file is missing")
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.coverImage[0]?.path;
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "coverImage file is missing");
   }
-  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-  if (!coverImage.url){
-    throw new ApiError(400,"Error while uploading file on cloudinary")
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!coverImage.url) {
+    throw new ApiError(400, "Error while uploading file on cloudinary");
   }
-  const user = await User.findByIdAndUpdate(req.user._id,{
-    $set:{coverImage:coverImage.url}
-  },{new:true}).select("-password -refreshToken")
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: { coverImage: coverImage.url },
+    },
+    { new: true }
+  ).select("-password -refreshToken");
 
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover Image Update successfully"));
+});
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+  if (!username?.trim()) {
+    throw new ApiError(400, "username is missing");
+  }
+  const channel = await User.aggregate([
+    {
+      $match: { username: username?.toLowerCase() },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscribers",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        sunscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+        isSubscribed: 1,
+      },
+    },
+  ]);
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exists");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], "User Channel fetched successfully")
+    );
+});
+
+const getMyWatchHistory = asyncHandler(async (req, res) => {
+  const myWatchHistoryData = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+
+    {$lookup:{
+        from:"videos",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as: "watchHistory"
+    }},
+    {$unwind:"$watchHistory"},
+    {$lookup:{
+        from:"users",
+        localField:"watchHistory.owner",
+        foreignField:"_id",
+        as: "watchHistory.owner"
+
+    }},
+      {
+        $addFields:{"watchHistory.owner":{$first:"$watchHistory.owner"}}   //array -> object
+      },
+
+      {$project:{
+        "watchHistory.title":1,
+        "watchHistory.description":1,
+        "watchHistory.thumbanil":1,
+        "watchHistory.duration":1,
+        "watchHistory.views":1,
+        "watchHistory.owner.fullName":1,
+        "watchHistory.owner.username":1,
+        "watchHistory.avatar":1,
+       
+        
+      }},
+      {$group:{
+        _id: new mongoose.Types.ObjectId(req.user._id),
+        totalWatchedVideos:{$sum:1},
+        watchHistory:{$push:"$watchHistory"},
+        
+      }}
+
+
+   
+  ]);
 
   return res
   .status(200)
-  .json(new ApiResponse(200,user,"Cover Image Update successfully"))
-})
+  .json(new ApiResponse(200,myWatchHistoryData,"watch history successfully fetched"))
+});
 
 export {
   registerUser,
@@ -327,5 +455,7 @@ export {
   changeCurrentPassword,
   updateAccountDetails,
   updateUserAvatar,
-  updateUserCoverImage
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getMyWatchHistory
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,24 +1,50 @@
 import { Router } from "express";
-import { registerUser,logoutUser,loginUser, refreshAccessToken } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import {
+  registerUser,
+  logoutUser,
+  loginUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserCoverImage,
+  updateUserAvatar,
+  getUserChannelProfile,
+  getMyWatchHistory,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
-import {verifyJWT}from "../middlewares/auth.middlewares.js"
-
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 router.route("/register").post(
-    upload.fields([{
-        name:"avatar",
-        maxCount:1
-    },{
-        name:"coverImage",
-        maxCount:1
-    }]),
-    registerUser)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT,logoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watchHistory").get(verifyJWT, getMyWatchHistory);
 
 export default router;
